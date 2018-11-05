8bbda6780499adbbd8b7cdb20e0c337f8b27a504
gaborkallos <gaborkallos@gmail.com>
2018-11-05 11:04:55 +0100

    change to h1

--- static/javascript/changeh1.js
+++ static/javascript/changeh1.js
@@ -2,10 +2,18 @@ function changeH1() {
     let rows = document.querySelectorAll('tbody tr');
     let openTag = '<h1>';
     let closeTag = '</h1>';
-    for (row of rows) {
-        row.addEventListener('click', function () {
-            this.innerHTML = openTag + this.innerHTML + closeTag;
-            console.log(this.innerHTML);
+    let name = document.querySelectorAll('#name');
+    let email = document.querySelectorAll('#email');
+    for (let i = 0; i < rows.length; i++) {
+        rows[i].addEventListener('click', function () {
+            console.log(this);
+            if (!this.innerHTML.includes('h1')) {
+                name[i].innerHTML = openTag + name[i].innerHTML + closeTag;
+                email[i].innerHTML = openTag + email[i].innerHTML + closeTag;
+            }else{
+                name[i].innerHTML = name[i].innerHTML.slice(4) + name[i].innerHTML.slice(-5)
+                email[i].innerHTML = email[i].innerHTML.slice(4) + email[i].innerHTML.slice(-5)
+            }
         })
     }
 }
