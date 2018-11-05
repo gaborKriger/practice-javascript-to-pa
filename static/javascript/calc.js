d48b7b82e1b0cad3f25a93d5824eebe274e8a724
gaborkallos <gaborkallos@gmail.com>
2018-11-05 13:48:58 +0100

    minor changes in calc

--- static/javascript/calc.js
+++ static/javascript/calc.js
@@ -4,12 +4,15 @@ function reply_click(clicked_id) {
 
 
 function buttons(clicked_id) {
-    let screen = document.querySelector('#screen');
-
+    let screen = document.querySelector('h1');
     if (clicked_id != '=') {
-
+        if (screen.innerText == '0') {
+            screen.innerText = ''
+        }
         screen.innerText += clicked_id;
-
+        if (clicked_id == 'C') {
+            screen.innerText = '0';
+        }
     } else {
 
         let result = eval(screen.innerText);
